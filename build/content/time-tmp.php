36f3fc6aa6a34884bb0a3bbfa8f261b495df493b
Igor MoonLite <igor.moonlite@gmail.com>
2023-11-19 08:12:29 +0300

    TruckersMP clock is now fully autonomous, no longer requires web requests

--- build/content/time-tmp.php
+++
@@ -1,5 +0,0 @@
-<?php
-$json = file_get_contents("https://api.truckersmp.com/v2/game_time");
-$data = json_decode($json);
-$resp = ["error"=>false,"time"=>strval($data->game_time),"fetch"=>time()];
-echo json_encode($resp);
